5bf761c8d0a9c9835afcdbc3301033ea6bab5ddf
amjadkhan730 <amjadktk730@gmail.com>
2022-12-21 10:29:57 -0800

    function

---
+++ function.py
@@ -0,0 +1,56 @@
+#Function 
+          #Function is block of code disgn to perform a particular task,,, whenever it is called,,,
+          
+          #There are two type of function
+         
+         # 1= Built - in function,
+                                 #Those function are defined and pre-coded in python,,Some example of built-in function,,
+                                 
+                                 #min(),,max(),,list(),,sum(),,type(),,print(),,
+                                 
+         # 2= User defined function,
+                            
+                                  # We can create function  to specific task as per our needs such function is user defined function,,
+                                  
+                       #Example
+                    
+# a = 9
+# b = 8
+# gmean1=(a*b)/(a+b)
+# print(gmean1) 
+
+# c=8
+# d=7
+# gmean2=(c*d)/(c+d)
+# print(gmean2)                                  
+ #def calculateGmean(a,b):
+ # print(a+b)
+# a=9
+# b=7
+# calculateGmean(a,b)
+
+# c=4
+# d=8
+# calculateGmean(c,d)
+
+# def calculateGmean(a,b):
+#      print(a+b)
+# a=8
+# b=6
+# if(a>b):
+#        print("First number is greater")
+# else:
+#   print("Second number is greater")
+#   calculateGmean(a,b)    
+# c=5
+# d=5
+# calculateGmean(c,d)
+
+
+
+          
+             
+
+
+
+
