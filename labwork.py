b2aa9389dd65fa4dcc25577ce25765f2809bada4
amjadkhan730 <amjadktk730@gmail.com>
2023-01-19 03:29:10 -0800

    nem adding data type

---
+++ labwork.py
@@ -0,0 +1,160 @@
+#data type
+
+# 1) python List=-
+                  #List is an ordered sequence of item or element,,
+
+  # Example=-                
+
+#list = [10,10.9 ,45,55,67,99,"tufail",11,15,"amjad"]
+#print(type(list),list)
+#print (list)
+#print (len(a))
+#print (a[3])
+#print (type(a))
+#Text = "my is Amjad and i am"+ age
+#age=20
+#print (text.format(ag)
+#list.insert(1, "khan")
+#print(list)
+#list.append("javascript")
+#print(list)
+#list.extend("1,2,3,4,55,6,77")
+#print(list)
+#list.remove("amjad")
+#print(list)
+#list.count("list") -
+#print(list)
+#list.reverse()
+#print(list)
+#list.pop(3)
+#print(list)
+#list.copy() -
+#print(list)
+#list.sort()
+#print(list)
+#list [3] = "phyton"
+#print(list)
+#print(list[0])
+#print(list[1])
+#print(list[2])
+#print(list[3])
+#print(list[4])
+#print(list[5])
+
+                #__________********_________#
+
+# 2)Tuple=-
+         #tuple are ordered collection of data  item,,They store multiple item in a single variable,,tuple item separated by comma and enclosed in round bracket,,Tuple are unchangeable,,
+         
+   #Example=
+
+#tup=(1,2,3,4,5,6,7,8,9,10)
+#tuple2=("amjad","khan","tufail","zaman")
+#print(tuple1)
+#print(tuple2)   
+#print(type(tuple1),tuple1)         
+#print(tuple1[0])
+#print(tuple1[1])
+#print(tuple1[2])
+#print(tuple1[3])
+#print(tuple1[4])
+#print(tuple1[5])
+#print(tuple1[6])
+#print(tuple1[7])
+#print(tuple1[8])
+#print(tuple1[9])
+#print(len(tup))
+#if 6 in tup:
+     # print("yes 6 is present in tuple")      
+#if 20 in tup:
+ #     print("yes 20 is present in tuple")
+
+#else:
+# print("no 20 is not in tuple")
+
+                    #_________*********__________#
+        
+
+#String=-
+         # String in python are surrounding by either single or double quotation mark,,
+         
+# Example=-
+         #  "hello" 'hello'         
+
+#print("Hello")
+
+#str= "amjad","khan","tufail","rashid","arshad","sohail"
+#print(str)
+#print(len(str))
+#print("amjad" in str)
+#print("hafezz" in s)
+#print(str[1::4])
+#print(str[0])
+#print(str[1])
+#print(str[2])
+#print(str[3])
+#print(str[4])
+
+ #Looping throuh a string,,
+ 
+str1 = "Amjad khattak from karak" 
+for character in str1:
+   print(character)    
+      
+
+#Function 
+          #Function is block of code disgn to perform a particular task,,, whenever it is called,,,
+          
+          #There are two type of function
+         
+         # 1= Built - in function,
+                                 #Those function are defined and pre-coded in python,,Some example of built-in function,,
+                                 
+                                 #min(),,max(),,list(),,sum(),,type(),,print(),,
+                                 
+         # 2= User defined function,
+                            
+                                  # We can create function  to specific task as per our needs such function is user defined function,,
+                                  
+                       #Example
+                    
+# a = 9
+# b = 8
+# gmean1=(a*b)/(a+b)
+# print(gmean1) 
+
+# c=8
+# d=7
+# gmean2=(c*d)/(c+d)
+# print(gmean2)                                  
+ #def calculateGmean(a,b):
+ # print(a+b)
+# a=9
+# b=7
+# calculateGmean(a,b)
+
+# c=4
+# d=8
+# calculateGmean(c,d)
+
+# def calculateGmean(a,b):
+#      print(a+b)
+# a=8
+# b=6
+# if(a>b):
+#        print("First number is greater")
+# else:
+#   print("Second number is greater")
+#   calculateGmean(a,b)    
+# c=5
+# d=5
+# calculateGmean(c,d)
+
+
+
+          
+             
+
+
+
+
