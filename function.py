861a180a17551b9a151a8a5d64ade429e6908fac
amjadkhan730 <amjadktk730@gmail.com>
2023-01-22 11:11:03 -0800

    Addin function

---
+++ function.py
@@ -0,0 +1,81 @@
+#Function=- 
+          #Function is block of code design to perform a particular task,,, whenever it is called,,,
+          
+          #There are two type of function
+         
+         # 1= Built - in function,
+                                 #Those function are defined and pre-coded in python,,Some example of built-in function,,
+                                 
+                                 #min(),,max(),,list(),,sum(),,type(),,print(),,
+                                 
+         # 2= User defined function,
+                            
+                                  # We can create function  to specific task as per our needs such function is user defined function,,
+                                  
+                                  # you can pass data, known as parameter, into a function,
+                                 
+                                 # A function can return data as a result,
+                                 
+                # Creating a Function,
+                
+                    
+#def my_function():
+#   print("Hello word")
+
+
+            # Calling a Function,
+
+#def my_function():
+ #  print("Hello word")
+
+#my_function()
+
+              # Argument=-
+                           # Information can be passed into function as argument,
+                           
+#def my_function(fname):
+    #print(fname + "khattak")
+    
+    #my_function("Amjad")                           
+    #my_function("Tufail")
+          
+                         
+#def my_function(fname, lname):
+    #print(fname +"" + lname)
+    
+    #my_function("Amjad", "khan")                           
+             
+                         
+                               
+#a = 9
+#b = 8
+#gmean1=(a*b)/(a+b)
+#print(gmean1) 
+#c=8
+#d=7
+#gmean2=(c*d)/(c+d)
+#print(gmean2)                                  
+#def calculateGmean(a,b):
+ #print(a+b)
+#a=9
+#b=7
+#calculateGmean(a,b)
+
+# c=4
+# d=8
+# calculateGmean(c,d)
+
+# def calculateGmean(a,b):
+#      print(a+b)
+# a=8
+# b=6
+# if(a>b):
+#        print("First number is greater")
+# else:
+#   print("Second number is greater")
+#   calculateGmean(a,b)    
+# c=5
+# d=5
+# calculateGmean(c,d)
+
+
